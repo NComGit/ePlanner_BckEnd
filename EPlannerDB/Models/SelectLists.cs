9c8a10b1d93f0af4eda96eddad299f17eb68cf2a
DrSloughKeg <bpye1995@gmail.com>
2024-03-25 22:06:49 -0400

    march 25 10pm

--- EPlannerDB/Models/SelectLists.cs
+++ EPlannerDB/Models/SelectLists.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace EPlannerDB.Models
 {
     public class SelectLists
     {
-        public SelectLists TodoItemsList { get; set; }
+        public SelectList TodoItemsList { get; set; }
 
-        //public SelectLists UsersList { get; set; }
+        //public SelectList UsersList { get; set; }
 
-        //public SelectLists TodoListList { get; set; }
+        //public SelectList TodoListList { get; set; }
     }
 }
